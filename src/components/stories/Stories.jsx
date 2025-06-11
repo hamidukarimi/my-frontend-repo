0b655ba66d21ff804b97341fc5fec906082643bd
Hamid Karimi <hameedukarimi@gmail.com>
2025-06-11 15:13:17 +0430

    animated placeholder added for story section

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -59,7 +59,6 @@
 
 // export default Stories;
 
-
 import React, { useEffect, useState } from "react";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 import axios from "axios";
@@ -68,30 +67,28 @@ import { faArrowLeft, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 const Stories = () => {
-   const [stories, setStories] = useState([]);
+  const [stories, setStories] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-
   // this is for only show the loged in user stories for himself
-//   useEffect(() => {
-//     const fetchStories = async () => {
-//   try {
-//     const token = localStorage.getItem("token");
-//     const response = await axios.get(`${BASE_URL}/api/stories`, {
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//       },
-//     });
-//     setStories(response.data);
-//   } catch (error) {
-//     console.error("Error fetching stories:", error);
-//   }
-// };
+  //   useEffect(() => {
+  //     const fetchStories = async () => {
+  //   try {
+  //     const token = localStorage.getItem("token");
+  //     const response = await axios.get(`${BASE_URL}/api/stories`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+  //     setStories(response.data);
+  //   } catch (error) {
+  //     console.error("Error fetching stories:", error);
+  //   }
+  // };
 
-
-//     fetchStories();
-//   }, []);
+  //     fetchStories();
+  //   }, []);
 
   useEffect(() => {
     const fetchStories = async () => {
@@ -117,25 +114,20 @@ const Stories = () => {
     fetchStories();
   }, []);
 
+  if (loading) {
+    return (
+      <section className="m-4 flex items-center gap-3 overflow-x-auto [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+        {[...Array(6)].map((_, index) => (
+          <div
+            key={index}
+            className=" w-[75px] h-[75px] bg-gray-300 animate-pulse rounded-2xl flex-shrink-0"
+          ></div>
+        ))}
+      </section>
+    );
+  }
+
   return (
-    // <div className="flex gap-4 overflow-x-auto p-4 bg-gray-100">
-    //   {stories.map((story) => (
-    //     <div
-    //       key={story._id}
-    //       className="min-w-[120px] flex-shrink-0 flex flex-col items-center"
-    //     >
-    //       <img
-    //         src={story.imagePath}
-    //         alt="Story"
-    //         className="w-20 h-20 rounded-full object-cover border-2 border-blue-500"
-    //       />
-    //       <p className="text-sm mt-2 text-center">{story.user?.username}</p>
-    //     </div>
-    //   ))}
-    // </div>
-
-
-
     <section className="m-4 flex items-center gap-3 overflow-x-auto [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
       <div className="w-[75px]">
         <Link to="/create-story">
@@ -149,22 +141,21 @@ const Stories = () => {
       </div>
 
       {[...stories].reverse().map((story) => (
-  <Link to={`/stories/${story._id}`} key={story._id}>
-    <div className="w-[75px]">
-      <div className="w-[75px] h-[75px] bg-placeholderColor border border-black rounded-2xl overflow-hidden">
-        <img
-          className="w-full h-full object-cover"
-          src={story.user?.profilePicture}
-          alt="profile"
-        />
-      </div>
-      <p className="truncate mt-1 text-center text-gray-800 text-sm">
-        {`${story.user?.name} ${story.user?.lastName || ""}`}
-      </p>
-    </div>
-  </Link>
-))}
-
+        <Link to={`/stories/${story._id}`} key={story._id}>
+          <div className="w-[75px]">
+            <div className="w-[75px] h-[75px] bg-placeholderColor border border-black rounded-2xl overflow-hidden">
+              <img
+                className="w-full h-full object-cover"
+                src={story.user?.profilePicture}
+                alt="profile"
+              />
+            </div>
+            <p className="truncate mt-1 text-center text-gray-800 text-sm">
+              {`${story.user?.name} ${story.user?.lastName || ""}`}
+            </p>
+          </div>
+        </Link>
+      ))}
     </section>
   );
 };
